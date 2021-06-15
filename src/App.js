39fc6e8d2a18fdd9fd8c8190ca583d6471432510
helfexrb23 <helbinrapheal23@gmail.com>
2021-06-16 00:23:01 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
+import {BrowserRouter as Router} from 'react-router-dom';
 import SearchPhoto from "./components/SearchPhoto";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <h1 className="title">React Photo Search</h1>
-        <SearchPhoto />
+    <Router basename = "/pikky-react/">
+      <div className="App">
+        <div className="container">
+          <h1 className="title">React Photo Search</h1>
+          <SearchPhoto />
+        </div>
       </div>
-    </div>
+    </Router>
+
   );
 }
 export default App;
